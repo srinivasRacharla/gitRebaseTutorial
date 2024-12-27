2a5ead81a871d4defb5e40e5ea56ff78dfc22c2b
srinivasRacharla <srinivas.racharla@valuelabs.com>
2024-12-27 16:58:26 +0530

    update the greeting message

--- app/src/main/java/com/example/gitrebasetest/MainActivity.kt
+++ app/src/main/java/com/example/gitrebasetest/MainActivity.kt
@@ -21,7 +21,7 @@ class MainActivity : ComponentActivity() {
             GitRebaseTestTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     Greeting(
-                        name = "Android",
+                        name = "Android main commit one",
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
